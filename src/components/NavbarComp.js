71a8265f6e7feeec9e21bb09e8773ecacf44dd44
sahaadrita <adrita.saha@optimizeitsystems.com>
2021-11-10 18:04:19 +0530

    day1

---
+++ src/components/NavbarComp.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Navbar,Nav } from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home';
+import Posts from './Posts';
+
+
+export default class NavbarComp extends Component {
+    render() {
+        return (
+          <Router>
+            <div>
+                <Navbar bg="light" expand="lg">
+  
+   
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link as={Link}to ={"/home"}>Home</Nav.Link>
+        <Nav.Link as={Link}to ={"/posts"}>Posts</Nav.Link>
+        
+     </Nav>
+    </Navbar.Collapse>
+ 
+</Navbar>
+
+
+            </div>
+            <Home/>
+            <Posts/>
+            </Router>
+            
+        );
+    }
+  }
+    
+  
---
+++ src/components/Posts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Posts extends Component {
+    render() {
+        return (
+            <div>
+                <h1>posts</h1>
+            </div>
+        )
+    }
+}
